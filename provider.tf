ca8c00f388224b5931dac2ee8bbe3efe2fe1cb72
tianomonteiro <tiano.monteiro@gmail.com>
2020-12-29 15:36:55 -0300

    Update provider.tf
    .

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 variable "ibmcloud_api_key" {}
 
 provider "ibm" {
-  ibmcloud_api_key   = var.ibmcloud_api_key
+  ibmcloud_api_key   = "${var.ibmcloud_api_key}"
   generation         = 2
   region             = "us-south"
 }
