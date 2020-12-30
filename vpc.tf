4885207eea1d8c35a841404661acb1b1f7563fc8
tianomonteiro <tiano.monteiro@gmail.com>
2020-12-30 14:45:30 -0300

    Update vpc.tf
    .

--- vpc.tf
+++ vpc.tf
@@ -12,7 +12,7 @@ output "url" {
   value = "${var.url}"
 }
 
-output {
+output "resources" {
    resource_id = null_resource.echourl.id
    resource_name = "URL de Administração do uCloud"
    resource_controller_url =  "${var.url}"
