12a1795c6100938da70c1ada78dbbf02893e4c06
tianomonteiro <tiano.monteiro@gmail.com>
2020-12-31 10:04:05 -0300

    Update vpc.tf
    .

--- vpc.tf
+++ vpc.tf
@@ -16,8 +16,12 @@ output "cpd_url" {
   value = "${var.url}"
 }
 
-output "resources" {
-  resource_id = "${null_resource.echourl.id}"
-  resource_name = "URL de Administração do uCloud"
-  resource_controller_url =  "${var.url}"
-}
+output "resource_cloud" {
+		value = {
+			   "is_resource" = "true"
+			   "resource_controller_url" = "${var.url}"
+			   "resource_icon_url" = "https://globalcatalog.test.cloud.ibm.com/api/v1/1082e7d2-5e2f-0a11-a3bc-f88a8e1931fc/artifacts/cache/8c6189a76c7d346f6e807aaffb3bf643-public/terraform.svg"
+			   "resource_id" = "${null_resource.echourl.id}"
+			   "resource_name" = "uCloudAdminConsole"
+		}
+	}
