7ac520b1d8a08c8cf04c7fda71b9fa864be39d6e
Christiano Monteiro <chrism@br.ibm.com>
2021-07-30 20:41:19 -0300

    att

--- vpc.tf
+++ vpc.tf
@@ -31,7 +31,7 @@ resource "null_resource" "curlurl" {
 
   provisioner "local-exec" {
     command = <<EOF
-    TOKEN=('curl --location --request POST 'https://iam.cloud.ibm.com/identity/token' --header 'Content-Type: application/x-www-form-urlencoded' --header 'Accept: application/json' --data-urlencode 'grant_type=urn:ibm:params:oauth:grant-type:apikey' --data-urlencode 'apikey=${var.ibmcloud_iam_key}' | awk -F\" '/access_token/{print $4}'')
+    export TOKEN=$('curl --location --request POST 'https://iam.cloud.ibm.com/identity/token' --header 'Content-Type: application/x-www-form-urlencoded' --header 'Accept: application/json' --data-urlencode 'grant_type=urn:ibm:params:oauth:grant-type:apikey' --data-urlencode 'apikey=${var.ibmcloud_iam_key}' | awk -F\" '/access_token/{print $4}'')
 	echo $TOKEN
     curl --location --request POST 'https://support-center.cloud.ibm.com/case-management/v1/cases' --header 'Content-Type: application/json' --header 'Authorization: Bearer $TOKEN' --data-raw '{ "type": "technical",
   "subject": "Case subject API Test",
